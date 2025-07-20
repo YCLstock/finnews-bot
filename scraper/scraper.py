73d35c5e80564cbc551bc21aa4cabaa75c5601e3
YCLstock <eric1504b@gmail.com>
2025-07-20 13:26:46 +0800

    修改webdriver

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,12 +8,10 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-from webdriver_manager.chrome import ChromeDriverManager
 
 from core.config import settings
 from core.database import db_manager
